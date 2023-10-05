239328b168c9aee234066d5a76ca457ef26bb3c4
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-10-05 18:56:21 +0000

    c programming again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main() {
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a specific string with double quotes and a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
   puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+  return (0);
 }
