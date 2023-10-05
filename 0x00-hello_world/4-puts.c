f4cf22ed3bd5a489014fc6bde339cbe91b3ee250
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-10-05 18:54:36 +0000

    c programming again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-  puts("\"Programming is like building a multilingual puzzle, followed by a new line");
+  puts("\"Programming is like building a multilingual puzzle");
   return 0;
 }
