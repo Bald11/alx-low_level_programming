96be84f2afdc04395e670a7657e87fdff267cded
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-10-10 15:27:13 +0000

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+int num = 0;
+while (num < 10)
+{
+printf("%d",num);
+num++;
+}
+printf("\n");
+return (0);
+}
