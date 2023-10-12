e9adfc06d0c2894d97b1bdad494f725cbd01870e
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-10-12 13:49:31 +0000

    a to z 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - Print the lowercase alphabet from 'a' to 'z', 10 times.
+ * print_alphabet_x10 - Print the lowercase alphabet from 'a' to 'z', 10 times.
  */
 
 void print_alphabet_x10(void)
