5f65071eb91d6062480a3c2234cf53b7d5b7ce34
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-10-12 11:39:22 +0000

    printing

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - The main function
+ *
+ * Description: This is the entry point for the program.
+ *
+ * Return: 0 on success.
+ */
+
+int main(void)
+{
+char name = "_putchar";
+putchar(name);
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+function n nested loops
