d462d68231ef0f1a427e2e27876662181cd0303c
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-10-12 12:57:07 +0000

    more functions

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,4 +5,17 @@
 
 #define myprint() printf("_putchar\n")
 
+void print_alphabet(void)
+{
+  char alpha = 'a';
+  int count = 0;
+  while(count < 26)
+    {
+      putchar(alpha);
+      count++;
+      alpha++;
+    }
+  printf("\n");
+}
+
 #endif
