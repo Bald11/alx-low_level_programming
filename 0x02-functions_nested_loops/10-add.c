224ea9e728192ed18c59ded3423fc6851798815e
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-10-13 18:29:29 +0000

    adding numbers to eachother

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integars
+ *
+ * @i: gets added
+ * @j: gets added
+ *
+ * Return: return the result
+ */
+
+int add(int i, int j)
+{
+int x = i + j;
+return (x);
+}
