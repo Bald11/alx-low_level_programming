a410e55c637c65009a3a50f48b4f7dfeb11b6f91
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-10-16 17:26:15 +0000

    pointers >:(

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - function that takes a pointer
+  * @n: integer pointer
+  * Return: returns n
+  */
+
+void reset_to_98(int *n)
+{
+int x = 98;
+*n = x;
+}
