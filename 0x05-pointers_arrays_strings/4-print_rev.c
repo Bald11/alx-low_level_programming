c954f51f078de6fc72e6c664391020a4fdbafb06
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-10-16 19:27:00 +0000

    put it in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - a function that outputs a reversed the input
+ * @s: a character
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+int x = strlen(s);
+for (int = x - 1; i > 0; i--)
+{
+printf("%c", str[i]);
+}
+printf("\n");
+}
