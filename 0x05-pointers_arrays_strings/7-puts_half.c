b61eeef76205b6416b3a2f250d9196b30cb4320f
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-10-16 21:23:01 +0000

    print half the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - print out half of the passed string
+ * @str: a string
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+int x = 0;
+int n = strlen(str) / 2;
+int y = n + 1;
+while (x < y)
+{
+printf("%c", str[n]);
+n++;
+x++;
+}
+printf("\n");
+}
