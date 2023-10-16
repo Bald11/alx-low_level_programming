f84171398aa3f307900741cf17e0663cc216d665
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-10-16 18:26:55 +0000

    counting the length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - counts character passed by
+ * @s: character?
+ * Return: returns the characters counted
+ */
+
+int _strlen(char *s)
+{
+return strlen(s);
+}
