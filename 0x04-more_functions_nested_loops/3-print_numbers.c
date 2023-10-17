d4ef402819f1fc81a5d98626fe45f5e18b22dfc6
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-10-17 11:36:49 +0000

    print from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0-9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+int x = 0;
+while (x < 10)
+{
+putchar(x);
+x++;
+}
+putchar('\n');
+}
