cd12bd23c01b35e7c394d415fb7ceed45b7cb004
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-10-17 12:01:52 +0000

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints from 0 to 15, 10 times
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+int x = 0;
+int y = 0;
+while (x < 10)
+{
+while (y < 15)
+{
+printf("%d", y);
+y++;
+}
+_putchar("\n");
+x++;
+y = 0;
+}
+}
