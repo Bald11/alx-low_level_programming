3e7211d552045c4d8911e25585e7c977130d39e8
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-10-19 12:27:32 +0000

    str and cat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - combines two string with maximum number of characters
+ * @dest: char pointer
+ * @src: char pointer
+ * @n: integer
+ * Return: returns a combined string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+return strncat(dest,src,n);
+} 
