27b0916ba49c648d1574967b42e160af6d58533b
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-10-31 13:42:09 +0000

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - raise a number to a specified power
+ * @x: integer
+ * @y: integer
+ * Return: -1 if y is less than 0 else return the result of pow
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0) return (-1);
+return pow(x,y);
+}
