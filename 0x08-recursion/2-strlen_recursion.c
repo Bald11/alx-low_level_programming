a200939557b55f1035d738a12ccfd5e1805c0a60
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-10-31 13:05:56 +0000

    string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include <string.h>
+
+/**
+ * _strlen_recursion - counts the character in a string
+ * @s: char to pointer
+ * Return: returns the value
+ */
+
+int _strlen_recursion(char *s)
+{
+return (strlen(s));
+}
