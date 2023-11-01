3095b8e412e3c04a1c969e9babb168f3f53b7073
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-11-01 16:02:24 +0000

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ * _memcpy - I don't really know
+ * @dest: char to pointer
+ * @src: char to pointer
+ * @n: integer
+ * Return: return memcpy
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+return (memcpy(dest, src, n));
+}
