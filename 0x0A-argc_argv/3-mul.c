1e97e7d1dcbcd69244655f20df101eb1acd49f17
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-11-01 19:44:00 +0000

    multi args

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,16 +10,11 @@
 
 int main(int argc, char *argv[])
 {
-if (argc == 1 || argc > 3)
+if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-else
-{
-int num1 = atoi(argv[1]);
-int num2 = atoi(argv[2]);
-printf("%d\n", num1 * num2);
-}
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 return (0);
 }
