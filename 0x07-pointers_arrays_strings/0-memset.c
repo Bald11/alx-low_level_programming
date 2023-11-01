83a5ee886bbaba9b1e1a73286bb673ad3bac3e70
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-11-01 15:54:54 +0000

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ * _memset - i have no clue
+ * @s: char to pointer
+ * @b: char
+ * @n: integer
+ * Return: return memset
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+return (memset(s, b, n));
+}
