322fdf3b5073ba1f4e8e698cc16a452e42254b21
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-11-01 17:33:29 +0000

    argc and argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,11 +7,11 @@
  * Return: return 0
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 if (argc >= 1)
-{  
-printf("%s\n",argv[0]);
+{
+printf("%s\n", argv[0]);
 }
 return (0);
 }
