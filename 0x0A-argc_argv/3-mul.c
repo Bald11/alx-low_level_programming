47c2d75c2979303327202c840e17c3d9b7af3c58
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-11-01 19:33:42 +0000

    multiply args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the main function
+ * @argc: integer
+ * @argv: char to pointer
+ * Return: return 0
+ */
+
+int main(int argc, char *argv[])
+{
+if (argc == 1 || argc > 3)
+{
+printf("Error\n");
+}
+else
+{
+int num1 = atoi(argv[1]);
+int num2 = atoi(argv[2]);
+printf("%d\n", num1 * num2);
+}
+return (0);
+}
