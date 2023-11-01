4acd2f90e08c31de765b7cc8ebca940c298e5c98
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-11-01 16:58:27 +0000

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ * _strcpy - copies a string
+ * @dest: char to pointer
+ * @src: char to pointer
+ * Return: returns strcpy
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+return (strcpy(dest, src));
+}
