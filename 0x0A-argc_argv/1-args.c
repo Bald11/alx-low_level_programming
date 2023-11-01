aef33b4f52cb6edf72bc9d699d60b8527e59c16f
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-11-01 19:16:42 +0000

    work around

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * @argc: integer
+ * @argv: char to pointer
+ * Return: return 0
+ */
+
+int main(int argc, char *argv[])
+{
+if (argv[0])
+{
+printf("%d\n", argc);
+}
+else
+{
+printf("%d\n", argc);
+}
+return (0);
+}
