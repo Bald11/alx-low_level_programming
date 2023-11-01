6332139b5766288886301912b4ca1ab07bd82692
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-11-01 15:35:26 +0000

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,6 +2,28 @@
 #include <math.h>
 
 /**
+ * is_prime_recursive - Check if a number is prime using recursion.
+ * @n: The number to be checked.
+ * @i: The current divisor to test.
+ *
+ * Return: 1 if the number is prime, 0 otherwise.
+ */
+
+int is_prime_recursive(int n, int i)
+{
+if (n <= 1)
+return (0);
+
+if (i == 1)
+return (1);
+
+if (n % i == 0)
+return (0);
+
+return (is_prime_recursive(n, i - 1));
+}
+
+/**
  * is_prime_number - Check if a number is prime.
  * @n: The number to be checked.
  *
@@ -10,19 +32,11 @@
 
 int is_prime_number(int n)
 {
-int i;
-if (n <= 1)
-return (0);
 if (n <= 3)
-return (1);
+return (n > 1);
+
 if (n % 2 == 0 || n % 3 == 0)
 return (0);
-i = 5;
-while (i * i <= n)
-{
-if (n % i == 0 || n % (i + 2) == 0)
-return (0);
-i += 6;
-}
-return (1);
+
+return (is_prime_recursive(n, (int)sqrt(n)));
 }
