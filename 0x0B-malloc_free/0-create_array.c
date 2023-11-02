1439869b8ab296279d16ab9dd943f601b98e2d48
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-11-02 12:36:35 +0000

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,10 +11,9 @@ char *create_array(unsigned int size, char c)
 {
 if (size == 0)
 {
-retrun (NULL);
+return (NULL);
 }
-else
-{
+
 char *array = (char *)malloc(size * sizeof(char));
 if (array == NULL)
 {
@@ -26,7 +25,6 @@ unsigned int i;
 for (i = 0; i < size; i++)
 {
 array[i] = c;
-}
 }
 }
 return (array);
