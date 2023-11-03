b6878eadc64a7b751666eed167cdf2c3cc4008ab
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-11-03 12:57:24 +0000

    malloc cpy

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strdup - duplicates a string
+ * @str: char to pointer
+ * Return: NULL if it fails
+ */
+
+char *_strdup(char *str)
+{
+char *string;
+unsigned int size = strlen(str) + 1;
+if (str == NULL)
+{
+return (NULL);
+}
+string = (char *)malloc(sizeof(char) * size);
+printf("%s", string);
+if (string == NULL)
+{
+return (NULL);
+}
+return (0);
+}
