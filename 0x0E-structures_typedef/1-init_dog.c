88fa04907adb44e2362211184a7dd3c7adc9e17a
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-11-06 22:12:48 +0000

    initialize dog structure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * init_dog - a function to initialize vars
+ * @d: stuct to pointer
+ * @name: char to pointer
+ * @age: float
+ * @owner: char to pointer
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+*name = *name;
+age = age;
+*owner = *owner;
+}
