0d9f6aef28a2fb7f62f953769ef27d65f05cb174
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-11-07 00:11:53 +0000

    without em

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+#include <string.h>
+
+/**
+ * new_dog - main entry point
+ * @name: char to pointer
+ * @age: float
+ * @owner: char to pointer
+ * Return: depends
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *x = (dog_t *)malloc(sizeof(dog_t));
+if (x == NULL)
+{
+return (NULL);
+}
+x->name = strdup(name);
+x->owner = strdup(owner);
+x->age = age;
+return (x);
+}
