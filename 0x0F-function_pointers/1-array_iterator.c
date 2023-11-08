7265976d3f8ffe7056c9e910fa4c693775692e7a
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-11-08 20:14:53 +0000

    more function pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - main entry point
+ * @array: integer to pointer
+ * @size: size_t
+ * @action: function pointer
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned int i;
+int j;
+for (i = 0; i < size; i++)
+{
+j = array[i];
+action(j);
+}
+}
