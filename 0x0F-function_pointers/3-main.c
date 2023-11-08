cadb34e82bfc0247a5e42f26742563792fda4608
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-11-08 23:44:15 +0000

    brain fk

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,13 +15,20 @@ int (*calculate)(int, int) = get_op_func(argv[2]);
 int num1 = atoi(argv[1]);
 int num2 = atoi(argv[3]);
 int result = calculate(num1, num2);
-if (argc == 4)
-{
-printf("%d\n", result);
-}
-else if (argc > 4 || argc < 4)
+char i = *argv[2];
+if (!calculate || argc != 4)
 {
 printf("Error\n");
+exit(69);
+}
+if ((i == '/' || i == '%') && num2 == 0)
+{
+printf("Error\n");
+exit(70);
+}
+else
+{
+printf("%d\n", result);
 }
 return (0);
 }
