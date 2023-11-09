28f5e5aac483340f80127ac9548da11407c110e5
Yasser Barhdadi <yasser22baghdadi@hotmail.com>
2023-11-09 00:04:04 +0000

    im done

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,61 +2,61 @@
 #include "3-calc.h"
 
 /**
- * op_add - adds two integers
- * @a: integer
- * @b: integer
- * Return: result
+ * op_add - calculates the sum of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
  */
-
 int op_add(int a, int b)
 {
-return (a + b);
+  return (a + b);
 }
 
 /**
- * op_sub - subtracts a from b
- * @a: integer
- * @b: integer
- * Return: result
+ * op_sub - calculates the difference of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference of a and b
  */
-
 int op_sub(int a, int b)
 {
-return (a - b);
+  return (a - b);
 }
 
 /**
- * op_mul - multiplies a with b
- * @a: integer
- * @b: integer
- * Return: result
+ * op_mul - calculates the product of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product of a and b
  */
-
 int op_mul(int a, int b)
 {
-return (a * b);
+  return (a * b);
 }
 
 /**
- * op_div - devides a by b
- * @a: integer
- * @b: integer
- * Return: result
+ * op_div - calculates the division of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of the division of a and b
  */
-
 int op_div(int a, int b)
 {
-return (a / b);
+  return (a / b);
 }
 
 /**
- * op_mod - the remaining of the devision of a / b
- * @a: integer
- * @b: integer
- * Return: result
+ * op_mod - calculates the remainder of the division of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder of a divided b
  */
-
 int op_mod(int a, int b)
 {
-return (a % b);
+  return (a % b);
 }
